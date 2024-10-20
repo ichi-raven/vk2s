02c2a9ce7280f5728c522a677eb06486d2804313
ichi-raven <i17314@kagawa.kosen-ac.jp>
2024-10-21 01:09:10 +0900

    removed throwing exception from scene loader

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -243,7 +243,7 @@ namespace vk2s
         if (!pScene || pScene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !pScene->mRootNode)
         {
             std::cerr << "ERROR::ASSIMP::" << mImporter.GetErrorString() << "\n";
-            throw std::runtime_error("failed to load asset with assimp!");
+            //throw std::runtime_error("failed to load asset with assimp!");
             return;
         }
 
